1c0f574a11e7c9a600978797f30e94e159cc1ef6
diego <die.rios@duocuc.cl>
2024-06-27 20:14:56 -0400

    Revert "ahora si"
    This reverts commit 9e56998f127c67229644b020768cf224b1919c6e.

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import transaccion from '@components/transaccion'
-import datos from '@components/datos'
 
 export default function Home() {
   return (
@@ -18,12 +17,11 @@ export default function Home() {
           Get started by editing <code>pages/index.js</code>
         </p>
         <p>
-          hola que hace bro yo casi muriendo
+          hola que hace broooo
         </p>
       </main>
 
       <transaccion />
-      <datos />
       <Footer />
     </div>
   )
